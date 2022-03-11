031042f750720c60a0a16b87fbef6f5671cda472
Eugene Efremov <jhekasoft@gmail.com>
2022-03-11 21:53:52 +0200

    Add icons

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,274 @@ import Typography from '@mui/material/Typography';
 import Chip from '@mui/material/Chip';
 import Box from '@mui/material/Box';
 import Avatar from '@mui/material/Avatar';
+import SvgIcon from '@mui/material/SvgIcon';
 import TelegramIcon from '@mui/icons-material/Telegram';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { 
+  faGolang,
+  faJsSquare,
+  faNodeJs,
+  faLinux,
+  faPhp,
+  faJava
+} from '@fortawesome/free-brands-svg-icons'
+
+function FlutterIcon() {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1999 2474.2">
+      <linearGradient
+        id="a"
+        x1="-2115.003"
+        x2="-2115.003"
+        y1="1930.741"
+        y2="1929.741"
+        gradientTransform="matrix(380.4042 -380.4175 -53.104 -53.1021 908228.313 -700059.5)"
+        gradientUnits="userSpaceOnUse"
+      >
+        <stop offset="0.2" stopOpacity="0.15"></stop>
+        <stop offset="0.85" stopColor="#616161" stopOpacity="0.01"></stop>
+      </linearGradient>
+      <linearGradient
+        id="b"
+        x1="-2115.269"
+        x2="-2114.269"
+        y1="1946.356"
+        y2="1946.356"
+        gradientTransform="matrix(565.291 0 0 -380.9571 1196694.25 743476.563)"
+        gradientUnits="userSpaceOnUse"
+      >
+        <stop offset="0.2" stopOpacity="0.55"></stop>
+        <stop offset="0.85" stopColor="#616161" stopOpacity="0.01"></stop>
+      </linearGradient>
+      <path
+        fill="#42a5f5"
+        fillOpacity="0.8"
+        d="M381 1618L0 1237 1237.1 0H1999m0 1141.5h-761.9l-285.4 285.4 381 381"
+      ></path>
+      <path
+        fill="#0d47a1"
+        d="M951.7 2188.8l285.4 285.4H1999l-666.3-666.3"
+      ></path>
+      <path
+        fill="#42a5f5"
+        d="M571.6 1808.1L952 1427.6l380.4 380.4L952 2188.5z"
+      ></path>
+      <path
+        fill="url(#a)"
+        d="M952 2188.5l380.4-380.4 53.1 53.1-380.4 380.4z"
+      ></path>
+      <path fill="url(#b)" d="M951.7 2188.8l565.3-195.3-184.3-185.7"></path>
+    </svg>
+  );
+}
+
+function TypeScriptIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 512 512"
+    >
+      <rect width="512" height="512" fill="#3178c6" rx="50"></rect>
+      <path
+        fill="#fff"
+        fillRule="evenodd"
+        d="M317 407v50c8.1 4.2 18 7.3 29 9.4s23 3.1 35 3.1 23-1.1 34-3.4 20-6.1 28-11c8.1-5.3 15-12 19-21s7.1-19 7.1-32c0-9.1-1.4-17-4.1-24s-6.6-13-12-18c-5.1-5.3-11-10-18-14s-15-8.2-24-12c-6.6-2.7-12-5.3-18-7.9-5.2-2.6-9.7-5.2-13-7.8-3.7-2.7-6.5-5.5-8.5-8.4-2-3-3-6.3-3-10 0-3.4.89-6.5 2.7-9.3s4.3-5.1 7.5-7.1 7.2-3.5 12-4.6c4.7-1.1 9.9-1.6 16-1.6 4.2 0 8.6.31 13 .94 4.6.63 9.3 1.6 14 2.9 4.7 1.3 9.3 2.9 14 4.9 4.4 2 8.5 4.3 12 6.9v-47c-7.6-2.9-16-5.1-25-6.5s-19-2.1-31-2.1-23 1.3-34 3.8-20 6.5-28 12c-8.1 5.4-14 12-19 21-4.7 8.4-7 18-7 30 0 15 4.3 28 13 38 8.6 11 22 19 39 27 6.9 2.8 13 5.6 19 8.3s11 5.5 15 8.4c4.3 2.9 7.7 6.1 10 9.5 2.5 3.4 3.8 7.4 3.8 12 0 3.2-.78 6.2-2.3 9s-3.9 5.2-7.1 7.2-7.1 3.6-12 4.8c-4.7 1.1-10 1.7-17 1.7-11 0-22-1.9-32-5.7-11-3.8-21-9.5-30-17zm-84-123h64v-41H118v41h64v183h51z"
+        clipRule="evenodd"
+      ></path>
+    </svg>
+  );
+}
+
+function ScalaIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      preserveAspectRatio="xMinYMin meet"
+      viewBox="0 0 256 416"
+    >
+      <defs>
+        <linearGradient id="a" x1="0%" y1="50%" y2="50%">
+          <stop offset="0%" stopColor="#4F4F4F"></stop>
+          <stop offset="100%"></stop>
+        </linearGradient>
+        <linearGradient id="b" x1="0%" y1="50%" y2="50%">
+          <stop offset="0%" stopColor="#C40000"></stop>
+          <stop offset="100%" stopColor="red"></stop>
+        </linearGradient>
+      </defs>
+      <path
+        fill="url(#a)"
+        d="M0 288v-32c0-5.394 116.377-14.428 192.2-32 36.628 8.49 63.8 18.969 63.8 32v32c0 13.024-27.172 23.51-63.8 32C116.376 302.425 0 293.39 0 288"
+        transform="matrix(1 0 0 -1 0 544)"
+      ></path>
+      <path
+        fill="url(#a)"
+        d="M0 160v-32c0-5.394 116.377-14.428 192.2-32 36.628 8.49 63.8 18.969 63.8 32v32c0 13.024-27.172 23.51-63.8 32C116.376 174.425 0 165.39 0 160"
+        transform="matrix(1 0 0 -1 0 288)"
+      ></path>
+      <path
+        fill="url(#b)"
+        d="M0 224v-96c0 8 256 24 256 64v96c0-40-256-56-256-64"
+        transform="matrix(1 0 0 -1 0 416)"
+      ></path>
+      <path
+        fill="url(#b)"
+        d="M0 96V0c0 8 256 24 256 64v96c0-40-256-56-256-64"
+        transform="matrix(1 0 0 -1 0 160)"
+      ></path>
+      <path
+        fill="url(#b)"
+        d="M0 352v-96c0 8 256 24 256 64v96c0-40-256-56-256-64"
+        transform="matrix(1 0 0 -1 0 672)"
+      ></path>
+    </svg>
+  );
+}
+
+function LuaIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      x="0"
+      y="0"
+      version="1"
+      viewBox="0 0 947 947"
+      xmlSpace="preserve"
+    >
+      <path
+        fill="navy"
+        d="M835.5 473.6c0-199.8-162.2-362-362-362s-362 162.2-362 362 162.2 362 362 362 362-162.2 362-362"
+      ></path>
+      <path
+        fill="#FFF"
+        d="M729.5 323.6c0-58.5-47.5-106-106-106s-106 47.5-106 106 47.5 106 106 106 106-47.5 106-106"
+      ></path>
+      <path
+        fill="navy"
+        d="M941.5 111.5c0-58.5-47.5-106-106-106s-106 47.5-106 106 47.5 106 106 106 106-47.4 106-106"
+      ></path>
+      <g fill="#FFF">
+        <path d="M258.1 627.8h117.3v26.7H227.8V417h30.3v210.8zM515.5 654.5v-23.8c-16 22.5-31.9 31.3-57 31.3-33.2 0-54.4-18.2-54.4-46.6V483.8h27v120.9c0 20.5 13.7 33.6 35.2 33.6 28.3 0 46.6-22.8 46.6-57.7v-96.8h27v170.7h-24.4zM738.4 659.1c-8.8 2.3-13 2.9-18.6 2.9-17.6 0-26.1-7.8-28-25.1-19.2 17.6-36.5 25.1-58 25.1-34.5 0-56-19.5-56-50.5 0-22.2 10.1-37.5 30-45.6 10.4-4.2 16.3-5.5 54.7-10.4 21.5-2.6 28.3-7.5 28.3-18.9v-7.2c0-16.3-13.7-25.4-38.1-25.4-25.4 0-37.8 9.4-40.1 30.3h-27.4c.7-16.9 3.9-26.7 11.7-35.5 11.4-12.7 31.9-19.9 56.7-19.9 42 0 64.2 16.3 64.2 46.6v100.4c0 8.5 5.2 13.4 14.7 13.4 1.6 0 2.9 0 5.9-.7v20.5zm-47.6-89c-9.1 4.2-15 5.5-43.7 9.4-29 4.2-41.1 13.4-41.1 31.3 0 17.3 12.4 27.4 33.6 27.4 16 0 29.3-5.2 40.4-15.3 8.1-7.5 10.8-13 10.8-22.2v-30.6z"></path>
+      </g>
+      <path
+        fill="none"
+        stroke="gray"
+        strokeDasharray="40.847"
+        strokeMiterlimit="10"
+        strokeWidth="10.861"
+        d="M890.6 261c33.5 65.8 51 138.6 51 212.5 0 258.4-209.7 468.1-468.1 468.1S5.4 731.9 5.4 473.5C5.4 215.1 215.1 5.4 473.5 5.4c83.1 0 164.6 22.1 236.2 63.9"
+      ></path>
+    </svg>
+  );
+}
+
+function NextJsIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 207 124"
+    >
+      <g fill="none" fillRule="evenodd" stroke="none" strokeWidth="1">
+        <g fill="#000" fillRule="nonzero" transform="translate(-247 -138)">
+          <g transform="translate(247 138)">
+            <g>
+              <path d="M48.942 32.632h38.96v3.082H52.512v23.193h33.278v3.082H52.513v25.464h35.794v3.081H48.942V32.632zm42.45 0h4.139l18.343 25.464 18.749-25.464L158.124.287l-41.896 60.485 21.59 29.762h-4.302l-19.642-27.086L94.15 90.534h-4.22l21.751-29.762-20.29-28.14zm47.967 3.082v-3.082h44.397v3.082h-20.453v54.82h-3.571v-54.82h-20.373z"></path>
+              <path d="M0.202923647 32.6320058L4.66697141 32.6320058 66.2235778 124.303087 40.785176 90.5343303 3.93649086 37.0111732 3.77416185 90.5343303 0.202923647 90.5343303z"></path>
+            </g>
+            <path d="M183.397 86.523c.738 0 1.276-.563 1.276-1.29 0-.727-.538-1.29-1.276-1.29-.73 0-1.277.563-1.277 1.29 0 .727.547 1.29 1.277 1.29zm3.509-3.393c0 2.146 1.555 3.549 3.822 3.549 2.414 0 3.874-1.446 3.874-3.956v-8.837h-1.946v8.828c0 1.394-.704 2.138-1.946 2.138-1.112 0-1.867-.692-1.893-1.722h-1.911zm10.24-.113c.14 2.233 2.007 3.662 4.787 3.662 2.97 0 4.83-1.498 4.83-3.887 0-1.878-1.06-2.917-3.632-3.514l-1.38-.338c-1.634-.38-2.294-.891-2.294-1.783 0-1.125 1.025-1.86 2.563-1.86 1.459 0 2.466.718 2.649 1.869h1.893c-.113-2.103-1.971-3.583-4.516-3.583-2.737 0-4.56 1.48-4.56 3.704 0 1.835 1.033 2.926 3.3 3.454l1.616.39c1.659.389 2.388.96 2.388 1.912 0 1.108-1.146 1.913-2.71 1.913-1.676 0-2.84-.753-3.005-1.939h-1.928z"></path>
+          </g>
+        </g>
+      </g>
+    </svg>
+  );
+}
+
+function PythonIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      xmlnsXlink="http://www.w3.org/1999/xlink"
+      viewBox="0 0 110 110"
+      version="1"
+    >
+      <defs>
+        <linearGradient id="linearGradient11301">
+          <stop offset="0" stopColor="#ffe052" stopOpacity="1"></stop>
+          <stop offset="1" stopColor="#ffc331" stopOpacity="1"></stop>
+        </linearGradient>
+        <linearGradient
+          id="linearGradient11307"
+          x1="89.137"
+          x2="147.777"
+          y1="111.921"
+          y2="168.101"
+          gradientUnits="userSpaceOnUse"
+          xlinkHref="#linearGradient11301"
+        ></linearGradient>
+        <linearGradient id="linearGradient9515">
+          <stop offset="0" stopColor="#387eb8" stopOpacity="1"></stop>
+          <stop offset="1" stopColor="#366994" stopOpacity="1"></stop>
+        </linearGradient>
+        <linearGradient
+          id="linearGradient9521"
+          x1="55.549"
+          x2="110.149"
+          y1="77.07"
+          y2="131.853"
+          gradientUnits="userSpaceOnUse"
+          xlinkHref="#linearGradient9515"
+        ></linearGradient>
+      </defs>
+      <g transform="translate(-473.36 -251.725)">
+        <g
+          fillOpacity="1"
+          fillRule="nonzero"
+          stroke="none"
+          strokeDasharray="none"
+          strokeDashoffset="0"
+          strokeLinecap="butt"
+          strokeLinejoin="miter"
+          strokeMiterlimit="4"
+          strokeOpacity="1"
+          strokeWidth="1"
+          markerEnd="none"
+          markerMid="none"
+          markerStart="none"
+          color="#000"
+          transform="translate(428.423 184.256)"
+          visibility="visible"
+        >
+          <path
+            style={{ marker: "none" }}
+            fill="url(#linearGradient9521)"
+            d="M99.75 67.469c-28.032 0-26.281 12.156-26.281 12.156l.031 12.594h26.75V96H62.875s-17.938-2.034-17.938 26.25 15.657 27.281 15.657 27.281h9.343v-13.125s-.503-15.656 15.407-15.656h26.531s14.906.241 14.906-14.406V82.125s2.263-14.656-27.031-14.656zM85 75.938a4.808 4.808 0 014.813 4.812A4.808 4.808 0 0185 85.563a4.808 4.808 0 01-4.813-4.813A4.808 4.808 0 0185 75.937z"
+            display="inline"
+            opacity="1"
+            overflow="visible"
+          ></path>
+          <path
+            d="M100.546 177.315c28.032 0 26.281-12.156 26.281-12.156l-.03-12.594h-26.75v-3.781h37.374s17.938 2.034 17.938-26.25c0-28.285-15.657-27.282-15.657-27.282h-9.343v13.125s.503 15.657-15.407 15.657h-26.53s-14.907-.241-14.907 14.406v24.219s-2.263 14.656 27.031 14.656zm14.75-8.469a4.808 4.808 0 01-4.812-4.812 4.808 4.808 0 014.812-4.813 4.808 4.808 0 014.813 4.813 4.808 4.808 0 01-4.813 4.812z"
+            style={{ marker: "none" }}
+            fill="url(#linearGradient11307)"
+            display="inline"
+            opacity="1"
+            overflow="visible"
+          ></path>
+        </g>
+      </g>
+    </svg>
+  );
+}
+
+function JavaScriptIcon() {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 630 630">
+      <path fill="#f7df1e" d="M0 0H630V630H0z"></path>
+      <path d="M423.2 492.19c12.69 20.72 29.2 35.95 58.4 35.95 24.53 0 40.2-12.26 40.2-29.2 0-20.3-16.1-27.49-43.1-39.3l-14.8-6.35c-42.72-18.2-71.1-41-71.1-89.2 0-44.4 33.83-78.2 86.7-78.2 37.64 0 64.7 13.1 84.2 47.4l-46.1 29.6c-10.15-18.2-21.1-25.37-38.1-25.37-17.34 0-28.33 11-28.33 25.37 0 17.76 11 24.95 36.4 35.95l14.8 6.34c50.3 21.57 78.7 43.56 78.7 93 0 53.3-41.87 82.5-98.1 82.5-54.98 0-90.5-26.2-107.88-60.54zm-209.13 5.13c9.3 16.5 17.76 30.45 38.1 30.45 19.45 0 31.72-7.61 31.72-37.2v-201.3h59.2v202.1c0 61.3-35.94 89.2-88.4 89.2-47.4 0-74.85-24.53-88.81-54.075z"></path>
+    </svg>
+  );
+}
 
 export default function Index() {
   return (
@@ -25,14 +292,67 @@ export default function Index() {
         <p><Typography>Technologies that I prefer (technology stack):</Typography></p>
         <p>
           <Chip
-            icon={<TelegramIcon />}
+            icon={<SvgIcon><FontAwesomeIcon color="#49ACD7" icon={faGolang} /></SvgIcon>}
             label="Go"
-            sx={{
-              '& .MuiSvgIcon-root': {
-                color: "#49ACD7"
-              }
-            }}
+          />&nbsp;
+          <Chip
+            icon={<SvgIcon><TypeScriptIcon /></SvgIcon>}
+            label="TypeScript"
+          />&nbsp;
+          <Chip
+            icon={<SvgIcon><FontAwesomeIcon color="#E7CF21" icon={faJsSquare} /></SvgIcon>}
+            label="JavaScript"
+          />&nbsp;
+          <Chip
+            icon={<SvgIcon><FontAwesomeIcon color="#639A58" icon={faNodeJs} /></SvgIcon>}
+            label="Node.js"
+          />&nbsp;
+          <Chip
+            icon={<SvgIcon><NextJsIcon /></SvgIcon>}
+            label="Next.js"
+          />&nbsp;
+          <Chip
+            icon={<SvgIcon><FontAwesomeIcon color="#000" icon={faLinux} /></SvgIcon>}
+            label="GNU/Linux"
           />
+          {/* <Chip
+            icon={<TelegramIcon sx={{ "&&": { color: "#49ACD7" } }} />}
+            label="Go"
+          /> */}
+        </p>
+
+        <p><Typography>Technologies that I used:</Typography></p>
+        <p>
+          <Chip
+            icon={<SvgIcon><ScalaIcon /></SvgIcon>}
+            label="Scala"
+          />&nbsp;
+          <Chip
+            icon={<SvgIcon><FlutterIcon /></SvgIcon>}
+            label="Flutter"
+          />&nbsp;
+          <Chip
+            label="C"
+          />&nbsp;
+          <Chip
+            label="C++"
+          />&nbsp;
+          <Chip
+            icon={<SvgIcon><LuaIcon /></SvgIcon>}
+            label="Lua"
+          />&nbsp;
+          <Chip
+            icon={<SvgIcon><PythonIcon /></SvgIcon>}
+            label="Python"
+          />&nbsp;
+          <Chip
+            icon={<SvgIcon><FontAwesomeIcon color="#777BB3" icon={faPhp} /></SvgIcon>}
+            label="PHP"
+          />&nbsp;
+          <Chip
+            icon={<SvgIcon><FontAwesomeIcon color="#5382a1" icon={faJava} /></SvgIcon>}
+            label="Java"
+          />&nbsp;
         </p>
       </Box>
     </Container>
